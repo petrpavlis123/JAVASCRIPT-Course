176800c182be35fed864491beac0ee51ce331b8e
Petr Pavlis <petrpavlis123@gmail.com>
2024-07-15 23:28:03 +0200

    ještě všechno nefunguije

--- .vscode/11-todo-list.js
+++ .vscode/11-todo-list.js
@@ -1,7 +1,12 @@
-const todoList = [];
-function addTodo() { 
-const imputElement = document.querySelector ('.js-name-input');
-const name = inputElememt.value;
-console.log (name);
+function addTodo() {
+  const input = document.querySelector('.js-name-input');
+  const todoList = document.getElementById('todo-list');
 
+  const todoText = input.value.trim();
+  if (todoText !== '') {
+    const todoItem = document.createElement('p');
+    todoItem.textContent = todoText;
+    todoList.appendChild(todoItem);
+    input.value = ''; // Clear the input field
+  }
 }
